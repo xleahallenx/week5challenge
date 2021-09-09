bf9eea642c0c60741f0ca61db4b9d02ac48d7f09
Leah <leah@Jamess-MBP.attlocal.net>
2021-09-08 20:52:37 -0500

    just getting started

---
+++ Develop/script.js
@@ -0,0 +1,5 @@
+//1. Display today's date
+//2. create (build) a single row for a task (hour label, input box, save button)
+//3. repeat this from 9 am to 5 pm (javascript?)
+//4. color code the different cells
+//5. save functions
